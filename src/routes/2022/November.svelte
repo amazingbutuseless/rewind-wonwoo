f8e68844260e2bca5770709421eb71d600eb44a0
amazingbutuseless <cindian@amazingbutuseless.net>
2023-02-01 22:29:12 +0900

    feat: 11월 타임라인 추가

---
+++ src/routes/2022/November.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Day from '$lib/Day.svelte';
+	import Link from '$lib/Link.svelte';
+	import Month from '$lib/Month.svelte';
+	import November18 from './November18.svelte';
+</script>
+
+<Month
+	month="November"
+	--month-font-color="var(--purple)"
+	--day-bg-color="var(--purple)"
+	--day-font-color="var(--black)"
+>
+	<Day day={26} datetime="2022-11-26">SVT World Tour; Be The Sun - Tokyo, Japan (26, 27 양일)</Day>
+
+	<Day day={19} datetime="2022-11-19">SVT World Tour; Be The Sun - Osaka, Japan (19, 20 양일)</Day>
+
+	<November18 />
+
+	<Day day={17} datetime="2022-11-17">DREAM, Fallin' Flower 발매 기념 악수회, 배웅회</Day>
+
+	<Day day={15} datetime="2022-11-15">DREAM 발매 기념 배웅회</Day>
+
+	<Day day={9} datetime="2022-11-09">SEVENTEEN JAPAN 1ST EP DREAM 발매</Day>
+
+	<Day day={8} datetime="2022-11-08">ODE TO YOU DVD 발매 기념 하이터치회</Day>
+
+	<Day day={7} datetime="2022-11-07">
+		CDTVライブ！ライブ！
+		<Link type="twitter" url="https://twitter.com/pledis_17jp/status/1589591229406523393" />
+	</Day>
+
+	<Day day={2} datetime="2022-11-02">
+		제9회 이데일리 문화대상 시상식 - 콘서트 부문 최우수상 수상
+	</Day>
+</Month>
---
+++ src/routes/2022/November18.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Link from '$lib/Link.svelte';
+</script>
+
+<article id="2022-11-18" class="flex flex-col justify-between my-8 p-8 h-screen box-border">
+	<time datetime="2022-11-18" class="date">18</time>
+	<h3 class="m-0 p-0 text-center">
+		<a
+			href="https://www.instagram.com/p/ClGSQQqPnca/?utm_source=ig_web_copy_link"
+			target="_blank"
+			rel="noopener noreferrer"
+			class="inline-block p-3"
+		>
+			헉슬리의 뮤즈가 되었습니다😄
+		</a>
+	</h3>
+	<div>
+		<ul>
+			<li>
+				HUXLEY ♡ SEVENTEEN WONWOO (Korean Ver.)
+				<Link type="youtube" url="https://www.youtube.com/shorts/c3h0SiRF6Zk" />
+			</li>
+			<li>
+				인사이드 세븐틴; 원우 헉슬리 광고 촬영 비하인드
+				<Link type="youtube" url="https://www.youtube.com/watch?v=aHGBtDor0s8" />
+			</li>
+		</ul>
+	</div>
+</article>
+
+<style lang="postcss">
+	article {
+		font-size: 1.6rem;
+	}
+
+	h3 a {
+		background-color: var(--white);
+	}
+</style>
